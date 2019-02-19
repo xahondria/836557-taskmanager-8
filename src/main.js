a004a268c660e3734cb68e9fec4a7eb450195ab7
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-02-19 16:16:39 +0300

    добавлен рендерер фильтров

---
+++ src/main.js
@@ -0,0 +1,173 @@
+'use strict';
+
+class Filter {
+  constructor() {
+    this.container = document.querySelector(`.filter`);
+
+    this.input = document.createElement(`input`);
+    this.label = document.createElement(`label`);
+
+    this.filtersProps = [
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__all`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: true,
+          disabled: false,
+        },
+        labelAttrs: {
+          for: `filter__all`,
+          class: `filter__label`,
+        },
+        labelText: `ALL`,
+        spanAttrs: {
+          class: `filter__all-count`,
+        },
+        spanText: `15`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__overdue`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: true,
+        },
+        labelAttrs: {
+          for: `filter__overdue`,
+          class: `filter__label`,
+        },
+        labelText: `OVERDUE`,
+        spanAttrs: {
+          class: `filter__overdue-count`,
+        },
+        spanText: `0`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__today`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: true,
+        },
+        labelAttrs: {
+          for: `filter__today`,
+          class: `filter__label`,
+        },
+        labelText: `TODAY`,
+        spanAttrs: {
+          class: `filter__today-count`,
+        },
+        spanText: `0`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__favorites`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: false,
+        },
+        labelAttrs: {
+          for: `filter__favorites`,
+          class: `filter__label`,
+        },
+        labelText: `FAVORITES`,
+        spanAttrs: {
+          class: `filter__favorites-count`,
+        },
+        spanText: `7`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__repeating`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: false,
+        },
+        labelAttrs: {
+          for: `filter__repeating`,
+          class: `filter__label`,
+        },
+        labelText: `Repeating`,
+        spanAttrs: {
+          class: `filter__repeating-count`,
+        },
+        spanText: `2`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__tags`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: false,
+        },
+        labelAttrs: {
+          for: `filter__tags`,
+          class: `filter__label`,
+        },
+        labelText: `Tags`,
+        spanAttrs: {
+          class: `filter__tags-count`,
+        },
+        spanText: `6`,
+      },
+      {
+        inputAttrs: {
+          type: `radio`,
+          id: `filter__archive`,
+          class: `filter__input visually-hidden`,
+          name: `filter`,
+          checked: false,
+          disabled: false,
+        },
+        labelAttrs: {
+          for: `filter__archive`,
+          class: `filter__label`,
+        },
+        labelText: `ARCHIVE`,
+        spanAttrs: {
+          class: `filter__archive-count`,
+        },
+        spanText: `115`,
+      },
+    ];
+  }
+
+  setAttributes(el, attrs) {
+    for (let key in attrs) {
+      if (attrs.hasOwnProperty(key) && attrs[key] !== false) {
+        el.setAttribute(key, attrs[key]);
+      }
+    }
+  }
+
+  render() {
+    this.container.innerHTML = ``;
+    this.fragment = document.createDocumentFragment();
+    this.filtersProps.forEach((props) => {
+      const newInput = this.input.cloneNode(true);
+      this.setAttributes(newInput, props.inputAttrs);
+      const newLabel = this.label.cloneNode(true);
+      this.setAttributes(newLabel, props.labelAttrs);
+      newLabel.innerHTML = `${props.labelText}<span ${props.spanAttrs}>${props.spanText}</span>`;
+      this.fragment.appendChild(newInput);
+      this.fragment.appendChild(newLabel);
+    });
+    this.container.appendChild(this.fragment);
+  }
+
+}
+
+const filter = new Filter();
+filter.render();
