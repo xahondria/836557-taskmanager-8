cc26f948d98b18974c361e57b431ee6436d2b7a3
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-02-24 11:41:32 +0300

    добавлен модуль рендера карточки

--- src/components/card-renderer.js
+++ src/components/card-renderer.js
@@ -1,18 +1,12 @@
+function renderCard() {
+  const container = document.querySelector(`.board__tasks`);
+  container.innerHTML = ``;
 
+  const fragment = document.createDocumentFragment();
 
-class Card {
-  constructor() {
-    this.container = document.querySelector(`.board__tasks`);
-  }
-
-  render() {
-    this.container.innerHTML = ``;
-    this.fragment = document.createDocumentFragment();
-
-    this.filtersProps.forEach((props) => {
-      const newElement = document.createElement(`template`);
-      newElement.innerHTML = `
-          <article class="card card--edit card--black">
+  const newElement = document.createElement(`template`);
+  newElement.innerHTML = `
+          <article class="card card--blue">
             <form class="card__form" method="get">
               <div class="card__inner">
                 <div class="card__control">
@@ -31,7 +25,7 @@ class Card {
                 </div>
 
                 <div class="card__color-bar">
-                  <svg width="100%" height="10">
+                  <svg class="card__color-bar-wave" width="100%" height="10">
                     <use xlink:href="#wave"></use>
                   </svg>
                 </div>
@@ -42,9 +36,7 @@ class Card {
                       class="card__text"
                       placeholder="Start typing your text here..."
                       name="text"
-                    >
-                      This is example of new task, you can add picture, set date and time, add tags.
-                    </textarea>
+                    ></textarea>
                   </label>
                 </div>
 
@@ -83,53 +75,53 @@ class Card {
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-mo-1"
+                            id="repeat-mo-5"
                             name="repeat"
                             value="mo"
                           />
-                          <label class="card__repeat-day" for="repeat-mo-1"
+                          <label class="card__repeat-day" for="repeat-mo-5"
                             >mo</label
                           >
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-tu-1"
+                            id="repeat-tu-5"
                             name="repeat"
                             value="tu"
                             checked
                           />
-                          <label class="card__repeat-day" for="repeat-tu-1"
+                          <label class="card__repeat-day" for="repeat-tu-5"
                             >tu</label
                           >
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-we-1"
+                            id="repeat-we-5"
                             name="repeat"
                             value="we"
                           />
-                          <label class="card__repeat-day" for="repeat-we-1"
+                          <label class="card__repeat-day" for="repeat-we-5"
                             >we</label
                           >
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-th-1"
+                            id="repeat-th-5"
                             name="repeat"
                             value="th"
                           />
-                          <label class="card__repeat-day" for="repeat-th-1"
+                          <label class="card__repeat-day" for="repeat-th-5"
                             >th</label
                           >
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-fr-1"
+                            id="repeat-fr-5"
                             name="repeat"
                             value="fr"
                             checked
                           />
-                          <label class="card__repeat-day" for="repeat-fr-1"
+                          <label class="card__repeat-day" for="repeat-fr-5"
                             >fr</label
                           >
                           <input
@@ -137,20 +129,20 @@ class Card {
                             type="checkbox"
                             name="repeat"
                             value="sa"
-                            id="repeat-sa-1"
+                            id="repeat-sa-5"
                           />
-                          <label class="card__repeat-day" for="repeat-sa-1"
+                          <label class="card__repeat-day" for="repeat-sa-5"
                             >sa</label
                           >
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-su-1"
+                            id="repeat-su-5"
                             name="repeat"
                             value="su"
                             checked
                           />
-                          <label class="card__repeat-day" for="repeat-su-1"
+                          <label class="card__repeat-day" for="repeat-su-5"
                             >su</label
                           >
                         </div>
@@ -158,7 +150,52 @@ class Card {
                     </div>
 
                     <div class="card__hashtag">
-                      <div class="card__hashtag-list"></div>
+                      <div class="card__hashtag-list">
+                        <span class="card__hashtag-inner">
+                          <input
+                            type="hidden"
+                            name="hashtag"
+                            value="repeat"
+                            class="card__hashtag-hidden-input"
+                          />
+                          <button type="button" class="card__hashtag-name">
+                            #repeat
+                          </button>
+                          <button type="button" class="card__hashtag-delete">
+                            delete
+                          </button>
+                        </span>
+
+                        <span class="card__hashtag-inner">
+                          <input
+                            type="hidden"
+                            name="hashtag"
+                            value="repeat"
+                            class="card__hashtag-hidden-input"
+                          />
+                          <button type="button" class="card__hashtag-name">
+                            #cinema
+                          </button>
+                          <button type="button" class="card__hashtag-delete">
+                            delete
+                          </button>
+                        </span>
+
+                        <span class="card__hashtag-inner">
+                          <input
+                            type="hidden"
+                            name="hashtag"
+                            value="repeat"
+                            class="card__hashtag-hidden-input"
+                          />
+                          <button type="button" class="card__hashtag-name">
+                            #entertaiment
+                          </button>
+                          <button type="button" class="card__hashtag-delete">
+                            delete
+                          </button>
+                        </span>
+                      </div>
 
                       <label>
                         <input
@@ -189,62 +226,62 @@ class Card {
                     <div class="card__colors-wrap">
                       <input
                         type="radio"
-                        id="color-black-1"
+                        id="color-black-5"
                         class="card__color-input card__color-input--black visually-hidden"
                         name="color"
                         value="black"
-                        checked
                       />
                       <label
-                        for="color-black-1"
+                        for="color-black-5"
                         class="card__color card__color--black"
                         >black</label
                       >
                       <input
                         type="radio"
-                        id="color-yellow-1"
+                        id="color-yellow-5"
                         class="card__color-input card__color-input--yellow visually-hidden"
                         name="color"
                         value="yellow"
                       />
                       <label
-                        for="color-yellow-1"
+                        for="color-yellow-5"
                         class="card__color card__color--yellow"
                         >yellow</label
                       >
                       <input
                         type="radio"
-                        id="color-blue-1"
+                        id="color-blue-5"
                         class="card__color-input card__color-input--blue visually-hidden"
                         name="color"
                         value="blue"
                       />
                       <label
-                        for="color-blue-1"
+                        for="color-blue-5"
                         class="card__color card__color--blue"
                         >blue</label
                       >
                       <input
                         type="radio"
-                        id="color-green-1"
+                        id="color-green-5"
                         class="card__color-input card__color-input--green visually-hidden"
                         name="color"
                         value="green"
+                        checked
                       />
                       <label
-                        for="color-green-1"
+                        for="color-green-5"
                         class="card__color card__color--green"
                         >green</label
                       >
                       <input
                         type="radio"
-                        id="color-pink-1"
+                        id="color-pink-5"
                         class="card__color-input card__color-input--pink visually-hidden"
                         name="color"
                         value="pink"
                       />
                       <label
-                        for="color-pink-1"
+                        for="color-pink-5"
                         class="card__color card__color--pink"
                         >pink</label
                       >
@@ -260,13 +297,9 @@ class Card {
             </form>
           </article>
       `;
-      this.fragment.appendChild(newElement.content);
-    });
+  fragment.appendChild(newElement.content);
 
-    this.container.appendChild(this.fragment);
-  }
-
+  container.appendChild(fragment);
 }
 
-const card = new Card();
-card.render();
+export default renderCard;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-
 import renderFilters from './components/filter-renderer';
+import renderCards from './components/card-renderer';
 
 renderFilters();
+renderCards();
