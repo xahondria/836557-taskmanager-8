865204008ee438553bb36c00b767da1db2011b49
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-02-27 16:11:27 +0300

    внесены изменения по замечаниям

--- src/mock/cards-data.js
+++ src/mock/cards-data.js
@@ -1,48 +1,65 @@
-import {getRandomElements} from "../components/utils";
+import utils from "../components/utils";
 
-const title = [
-  `Изучить теорию`,
-  `Сделать домашку`,
-  `Пройти интенсив на соточку`,
-][Math.floor(Math.random() * 3)];
-
-const dueDate = Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000;
-
-const tagsList = new Set([
-  `homework`,
-  `theory`,
-  `practice`,
-  `intensive`,
-  `keks`,
-]);
-
-const tags = getRandomElements([...tagsList], 3);
-
-const picture = `http://picsum.photos/100/100?r=${Math.random()}`;
-
-const color = [`black`, `yellow`, `blue`, `green`, `pink`][Math.floor(Math.random() * 5)];
-
-const repeatingDays = {
-  Mo: Math.floor(Math.random() * 5) === 4,
-  Tu: Math.floor(Math.random() * 5) === 4,
-  We: Math.floor(Math.random() * 5) === 4,
-  Th: Math.floor(Math.random() * 5) === 4,
-  Fr: Math.floor(Math.random() * 5) === 4,
-  Sa: Math.floor(Math.random() * 5) === 4,
-  Su: Math.floor(Math.random() * 5) === 4,
+const DATA = {
+  titleList: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+  ],
+  DAY_TO_MS_RATE: 86400000,
+  DAYS_DISPERSION: 7,
+  tagsList: new Set([
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`,
+  ]),
+  colors: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ],
+  days: {
+    Mo: 123,
+    Tu: false,
+    We: false,
+    Th: false,
+    Fr: false,
+    Sa: false,
+    Su: false,
+  },
 };
 
+class Card {
+  constructor() {
+    this.title = DATA.titleList[utils.getRandomInt(2)];
+    this.dueDate = Date.now() + utils.getRandomInt(DATA.DAYS_DISPERSION) * DATA.DAY_TO_MS_RATE;
+    this.tags = utils.getRandomEltsFromArray([...DATA.tagsList], 3);
+    this.picture = `http://picsum.photos/100/100?r=${Math.random()}`;
+    this.color = DATA.colors[utils.getRandomInt(DATA.colors.length - 1)];
+    this.repeatingDays = this.setRepeatingDays();
+    this.isFavorite = Math.floor(Math.random() * 5) === 4;
+    this.isDone = Math.floor(Math.random() * 2) === 1;
+  }
+
+  setRepeatingDays() {
+    for (let prop in DATA.days) {
+      if (DATA.days.hasOwnProperty(prop)) {
+        DATA.days[prop] = utils.getRandomInt(5) === 5;
+      }
+    }
+  }
+}
+
+
 const CARDS_DATA = [
-  {
-    title,
-    dueDate,
-    tags,
-    picture,
-    color,
-    repeatingDays,
-    isFavorite: Math.floor(Math.random() * 5) === 4,
-    isDone: Math.floor(Math.random() * 2) === 1,
-  },
+  new Card(),
+  new Card(),
+  new Card(),
+  new Card(),
 ];
 
 export default CARDS_DATA;
